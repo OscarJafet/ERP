1401c93ca6e516e6939f3da36c4c03db786dedf3
gavil <gavil@Gaviles>
2018-09-07 11:52:05 -0500

    cambios2

--- src/LaboratorioOperaciones.java
+++ src/LaboratorioOperaciones.java
@@ -1,3 +1,14 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +20,146 @@
  * @author gavil
  */
 public class LaboratorioOperaciones {
+    
+    
+     Statement stn;
+      static ResultSet rs = null;
+     static JTable tableTem;
+     static String cadenaSQL;
+
+     
+    public void consultaTodosLaboratorios(JTable tabla, Connection con){
+       
+        DefaultTableModel tablaTemp = (DefaultTableModel) tabla.getModel();
+        cadenaSQL="select IDLABORATORIO,NOMBRE,ORIGEN,ESTATUS from laboratorios";
+           
+        try {
+            stn=(java.sql.Statement) con.createStatement();
+            rs=stn.executeQuery(cadenaSQL);
+        
+            while(rs.next()){
+                
+                 String X=rs.getString("IDLABORATORIO");
+                   String Y=rs.getString("NOMBRE");
+                     String A=rs.getString("ORIGEN");
+                      String J=rs.getString("ESTATUS");
+                       
+                Object datosRenglon[]={X,Y,A,J};
+                tablaTemp.addRow(datosRenglon);
+            }
+            tabla.setModel(tablaTemp);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+    
+    public void consultaEspecifica(JTable tabla, Connection con,Object dato[]){
+       
+        DefaultTableModel tablaTemp = (DefaultTableModel) tabla.getModel();
+        cadenaSQL="select IDLABORATORIO,NOMBRE,ORIGEN,ESTATUS from laboratorios where nombre='"+dato[1]+"";
+           
+        try {
+            stn=(java.sql.Statement) con.createStatement();
+            rs=stn.executeQuery(cadenaSQL);
+        
+            while(rs.next()){
+                
+                 String X=rs.getString("IDLABORATORIO");
+                   String Y=rs.getString("NOMBRE");
+                     String A=rs.getString("ORIGEN");
+                      String J=rs.getString("ESTATUS");
+                      
+                Object datosRenglon[]={X,Y,A,J};
+                tablaTemp.addRow(datosRenglon);
+            }
+            tabla.setModel(tablaTemp);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+      
+      
+ public void consultaregistroE(JTable tabla, Connection con){
+       
+        DefaultTableModel tablaTemp = (DefaultTableModel) tabla.getModel();
+        cadenaSQL="select IDUNIDAD,NOMBRE from unidadmedida";
+           
+        try {
+            stn=(java.sql.Statement) con.createStatement();
+            rs=stn.executeQuery(cadenaSQL);
+        
+            while(rs.next()){
+                
+                 String X=rs.getString("IDUNIDAD");
+                   String Y=rs.getString("NOMBRE");
+                    
+                       
+                Object datosRenglon[]={X,Y};
+                tablaTemp.addRow(datosRenglon);
+            }
+            tabla.setModel(tablaTemp);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+      public void insertaEmpaque(Object datos[]){
+       
+     cadenaSQL="insert into Empaques values(" +  datos[0]+",'" + datos[1]+"',"
+             +  datos[2]+"," + datos[3]+"," + datos[4]+")";
+        try {
+            stn.executeUpdate(cadenaSQL);
+            JOptionPane.showMessageDialog(null, "Datos Ingresados");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Emplaque existente");
+//            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }
+      
+         public void BajaEmpaque(Object dato[]){
+       
+ 
+        cadenaSQL="update empaques set estatus = "+dato[0]+" where idempaque="+dato[1];
+           
+        try {
+            stn.executeUpdate(cadenaSQL);
+            JOptionPane.showMessageDialog(null, "Empaque dado de baja");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+       
+           public void actualizarCantidad(Object dato[]){
+       
+ 
+        cadenaSQL="update empaques set  Capacidad="+dato[0]+" where idempaque="+dato[1];
+           
+        try {
+            stn.executeUpdate(cadenaSQL);
+            JOptionPane.showMessageDialog(null, "Actualizacion Correcta");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+           
+      public void Activar(Object dato[]){
+       
+ 
+        cadenaSQL="update empaques set estatus = "+dato[0]+" where idempaque="+dato[1];
+           
+        try {
+            stn.executeUpdate(cadenaSQL);
+            JOptionPane.showMessageDialog(null, "Empaque dado de Alta");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     }
+ 
     
 }
